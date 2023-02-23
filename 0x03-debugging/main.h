849d82c1f01ac04b15d05ffab6e87fd78db450da
birdieee <lolasimbiat7@gmail.com>
2023-02-23 04:51:49 -0800

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H */
