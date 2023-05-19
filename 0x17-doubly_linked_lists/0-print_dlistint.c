3ad33d1d97326a2b7f209ac088ef997ac629b378
birdieee <lolasimbiat@gmail.com>
2023-05-19 14:44:45 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: The head of the dlistint_t list
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
  * Return: The number of nodes in the list.
  */
